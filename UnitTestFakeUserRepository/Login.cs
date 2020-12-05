e0eafe9791b8ce01130b1e372d953f0e1a6f3e0d
1999ajf <anna.fields@trojans.dsu.edu>
2020-12-04 21:44:29 -0600

    Added Unit Tests for Login functionality.

---
+++ UnitTestFakeUserRepository/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OO_Final;
+
+namespace UnitTestFakeUserRepository
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod]
+        public void LoginWithGoodCredsPass()
+        {
+            // Arrange
+            const string name = "Jason";
+            const string pass = "Password1!";
+            FakeUserRepository UsrRepo = new FakeUserRepository();
+
+            // Act
+            bool result = UsrRepo.Login(name, pass);
+
+            // Assert
+            Assert.IsTrue(result == true);
+        }
+
+        [TestMethod]
+        public void LoginWithBadCredsFail()
+        {
+            // Arrange
+            const string name = "Test";
+            const string pass = "Test";
+            FakeUserRepository UsrRepo = new FakeUserRepository();
+
+            // Act
+            bool result = UsrRepo.Login(name, pass);
+
+            // Assert
+            Assert.IsTrue(result == false);
+        }
+    }
+}
---
+++ UnitTestFakeUserRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFakeUserRepository")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFakeUserRepository")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2ca2709-fdae-45b7-8dd6-15a33ee348bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
